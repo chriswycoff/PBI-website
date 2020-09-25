8d5105d5c039c01c8bff8278f35bda9f933714c7
chriswycoff <wycoffc@air-weigh.com>
2020-09-25 14:21:32 -0700

    updated publications

--- public/js/init.js
+++ public/js/init.js
@@ -1,11 +1,24 @@
-(function($){
-  $(function(){
+// (function($){
+//   $(function(){
 
-    $('.sidenav').sidenav();
-    $('.parallax').parallax();
-    var modals = document.querySelectorAll('.modal');
-    M.Modal.init(modals);
+//     $('.sidenav').sidenav();
+//     $('.parallax').parallax();
+//     var modals = document.querySelectorAll('.modal');
+//     M.Modal.init(modals);
 
-  }); // end of document ready
-})(jQuery); // end of jQuery name space
+//   }); // end of document ready
+// })(jQuery); // end of jQuery name space
 
+document.addEventListener('DOMContentLoaded', function() {
+
+  let modals = document.querySelectorAll('.modal');
+  M.Modal.init(modals);
+
+  let navs = document.querySelectorAll('.sidenav');
+  M.Sidenav.init(navs);
+
+  let parallaxs = document.querySelectorAll('.parallax');
+  M.Parallax.init(parallaxs);
+
+
+});
