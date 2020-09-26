085f9aab93bbf9eb5ff6af6362c7df79b5942800
chriswycoff <wycoffc@air-weigh.com>
2020-09-25 22:37:50 -0700

    sandboxing different effects

--- public/js/nav.js
+++ public/js/nav.js
@@ -14,3 +14,68 @@
 //     await aboutFunction();
 //     window.location.href = "#about";
 // }
+const reveal = {
+    origin : 'left',
+    delay    : 300,
+    distance : '120px',
+    easing   : 'ease-in-out',
+  };
+
+const reveal2 = {
+    origin : 'top',
+    delay    : 300,
+    distance : '120px',
+    easing   : 'ease-in-out',
+};
+
+
+// for publications
+window.sr = ScrollReveal();
+// sr.reveal('.pubRow');
+sr.reveal('.first', reveal);
+sr.reveal('.second', reveal2);
+
+const leftReveal= {
+    origin : 'left',
+    delay    : 150,
+    distance : '120px',
+    easing   : 'ease-in-out',
+    viewOffset: {
+        bottom: 0
+    }
+  };
+
+  const midReveal= {
+    origin : 'left',
+    delay    : 300,
+    distance : '120px',
+    easing   : 'ease-in-out',
+    viewOffset: {
+        bottom: 0
+    }
+  };
+
+  const rightReveal= {
+    origin : 'left',
+    delay    : 450,
+    distance : '120px',
+    easing   : 'ease-in-out',
+    viewOffset: {
+        bottom: 0
+    }
+  };
+
+  sr.reveal('.firstCol', leftReveal);
+  sr.reveal('.secondCol', midReveal);
+  sr.reveal('.thirdCol', rightReveal);
+
+
+  
+  const footerReveal= {
+    origin : 'bottom',
+    delay    : 100,
+    distance : '120px',
+    easing   : 'ease-in-out',
+  };
+
+  sr.reveal('.footerContent', footerReveal);
